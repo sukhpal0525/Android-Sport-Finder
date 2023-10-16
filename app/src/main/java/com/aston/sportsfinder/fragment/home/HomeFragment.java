509d68180cf8994967d879c362003b2a785c15c5
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-10-16 22:49:01 +0100

    fix: bottom navigation highlighting (via stack)

--- app/src/main/java/com/aston/sportsfinder/fragment/home/HomeFragment.java
+++ app/src/main/java/com/aston/sportsfinder/fragment/home/HomeFragment.java
@@ -7,32 +7,102 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.aston.sportsfinder.R;
 import com.aston.sportsfinder.databinding.FragmentHomeBinding;
 import com.aston.sportsfinder.model.home.HomeViewModel;
+import com.aston.sportsfinder.util.NavigationUtil;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.welcomeMessage;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+        view.findViewById(R.id.searchButton).setOnClickListener(v -> {
+            NavOptions navOptions = new NavOptions.Builder()
+                    .setPopUpTo(R.id.navigation_home, true)
+                    .build();
+            navController.navigate(R.id.action_navigation_home_to_search, null, navOptions);
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+//            BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.nav_view);
+//            NavigationUtil.deselectBottomNavItems(bottomNavigationView);
+            //            BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.nav_view);
+            //            bottomNavigationView.setSelectedItemId(R.id.navigation_search);
+
+
+
+//  new test
+//        view.findViewById(R.id.searchButton).setOnClickListener(v -> {
+//            Navigation.findNavController(v).navigate(R.id.action_navigation_home_to_search);
+//            BottomNavigationView bottomNav = getActivity().findViewById(R.id.nav_view);
+//            bottomNav.setSelectedItemId(R.id.navigation_search); // Update the selected item to searchFragment
+//        });
+
+
+//public class HomeFragment extends Fragment {
+//
+//    private FragmentHomeBinding binding;
+//
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//        HomeViewModel homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
+//
+//        binding = FragmentHomeBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.welcomeMessage;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
+//    }
+//
+//    public void onViewCreated(View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        view.findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Navigation.findNavController(view).navigate(R.id.navigation_search);
+//            }
+////
+////                BottomNavigationView bottomNav = getActivity().findViewById(R.id.nav_view);
+////                bottomNav.setSelectedItemId(R.id.navigation_search);
+////                // HOME Fragment ----> SEARCH Fragment
+////                NavHostFragment.findNavController(HomeFragment.this)
+////                        .navigate(R.id.action_HomeFragment_to_SearchFragment);
+//        });
+//    }
+//
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
+//}
