50957c75210c77be779e688bafe1775c2cb20050
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-10-16 22:50:35 +0100

    fix: overridden navigation due to custom nav graph entries

--- app/src/main/java/com/aston/sportsfinder/MainActivity.java
+++ app/src/main/java/com/aston/sportsfinder/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aston.sportsfinder;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,22 +15,44 @@ import com.aston.sportsfinder.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            Log.d("NAVIGATION", "Navigated to: " + destination.getLabel());
+        });
     }
-
 }
+
+
+
+//    private ActivityMainBinding binding;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+////        BottomNavigationView navView = binding.navView;
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_search, R.id.navigation_profile)
+//                .build();
+//
+////      NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+////      NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+////      NavigationUI.setupWithNavController(navView, navController);
+//
+//        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupWithNavController(bottomNav, navController);
+//
+//    }
+//}
