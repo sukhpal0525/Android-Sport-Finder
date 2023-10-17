1fbb4f0879c10910ded89b567acbca6a0f4803c3
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-10-17 20:35:41 +0100

    chore: modify certain views in fragment_home

--- app/src/main/java/com/aston/sportsfinder/util/NavigationUtil.java
+++ app/src/main/java/com/aston/sportsfinder/util/NavigationUtil.java
@@ -4,7 +4,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavigationUtil {
 
-    //Deselect bottom navigation highlighting
+    // Deselect bottom navigation highlighting
     public static void deselectBottomNavItems(BottomNavigationView bottomNavView) {
         for (int i = 0; i < bottomNavView.getMenu().size(); i++) {
             bottomNavView.getMenu().getItem(i).setChecked(false);
