941d0a47983ac60e67deb029980a99e421dcef45
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-01 13:40:53 +0000

    feat: add important entity classes

---
+++ app/src/main/java/com/aston/sportsfinder/model/Location.java
@@ -0,0 +1,54 @@
+package com.aston.sportsfinder.model;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+
+    //    private Street street;
+    private String city;
+    private String state;
+    private String country;
+    private String postcode;
+    private Coordinates coordinates;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+}
---
+++ app/src/main/java/com/aston/sportsfinder/model/Street.java
@@ -0,0 +1,25 @@
+//package com.aston.sportsfinder.model;
+//
+//import java.io.Serializable;
+//
+//public class Street implements Serializable {
+//
+//    private int number;
+//    private String name;
+//
+//    public int getNumber() {
+//        return number;
+//    }
+//
+//    public void setNumber(int number) {
+//        this.number = number;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
