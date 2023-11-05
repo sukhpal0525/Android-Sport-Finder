a0cb261c593c0b81e55158ceab2854b35b8570f7
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-05 01:12:32 +0000

    feature: extend map initialisation
    -  Fetch & display test games from the database.

--- app/src/main/java/com/aston/sportsfinder/fragment/search/SearchFragment.java
+++ app/src/main/java/com/aston/sportsfinder/fragment/search/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.aston.sportsfinder.fragment.search;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +11,16 @@ import androidx.fragment.app.Fragment;
 
 import com.aston.sportsfinder.R;
 import com.aston.sportsfinder.databinding.FragmentSearchBinding;
-import com.aston.sportsfinder.model.Coordinates;
 import com.aston.sportsfinder.model.Game;
-import com.aston.sportsfinder.model.Location;
+import com.aston.sportsfinder.util.DatabaseClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class SearchFragment extends Fragment implements OnMapReadyCallback {
 
@@ -37,7 +39,7 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        initializeGame();
+//      initializeGame();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -45,37 +47,29 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-
-        Coordinates c = game.getLocation().getCoordinates();
-        LatLng loc = new LatLng(c.getLatitude(), c.getLongitude());
-        mMap.addMarker(new MarkerOptions()
-                .position(loc)
-                .title(game.getTeam1() + " vs " + game.getTeam2()));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 10));
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        getGamesFromDatabase();
     }
 
-    private void initializeGame() {
-        game = new Game();
-        game.setId(1);
-        game.setGameType("FOOTBALL");
-        game.setTeam1("Team A");
-        game.setTeam2("Team B");
-        game.setDate("2021-10-12");
-        game.setDateTime("10:00 AM");
+    private void getGamesFromDatabase() {
+        DatabaseClient.getInstance(getContext()).executorService.execute(() -> {
+            List<Game> games = DatabaseClient.getInstance(getContext()).getAppDatabase().gameDao().getAllGames();
+            getActivity().runOnUiThread(() -> {
+                showGamesOnMap(games);
+                Log.d("ASTON", "Number of games fetched: " + games.size());
+            });
+        });
+    }
 
-        Location location = new Location();
-        location.setCity("London");
-        location.setCountry("UK");
-        location.setState("England");
-
-        Coordinates coordinates = new Coordinates();
-        coordinates.setLatitude(51.5074);
-        coordinates.setLongitude(-0.1278);
-        location.setCoordinates(coordinates);
-
-        game.setLocation(location);
+    private void showGamesOnMap(List<Game> games) {
+        for (Game game : games) {
+            LatLng loc = new LatLng(game.getLatitude(), game.getLongitude());
+            mMap.addMarker(new MarkerOptions()
+                    .position(loc)
+                    .title(game.getTeam1() + " vs " + game.getTeam2()));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 10));
+        }
+        mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 }
