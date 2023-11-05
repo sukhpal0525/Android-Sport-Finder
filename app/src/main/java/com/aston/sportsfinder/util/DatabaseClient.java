ee67288309a78d0b3075d0b5f7fdba1f824f7449
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-05 01:44:50 +0000

    feature: implement automatic table deletion & Executor
    - Enable multi-threading functionality by initialising a ExecutorService object in DatabaseClient
    - Refactor resetDatabase() to utilise GameDao.
    - Update constructor to initialise a new Room db instance, reset after migration, callback to DatabaseCallback, and call the reset method.

--- app/src/main/java/com/aston/sportsfinder/util/DatabaseClient.java
+++ app/src/main/java/com/aston/sportsfinder/util/DatabaseClient.java
@@ -3,17 +3,26 @@ package com.aston.sportsfinder.util;
 import android.content.Context;
 
 import androidx.room.Room;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.aston.sportsfinder.AppDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DatabaseClient {
 
     private static DatabaseClient instance;
     private AppDatabase appDatabase;
+    public final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     private DatabaseClient(Context context) {
-        appDatabase = Room.databaseBuilder(context, AppDatabase.class, "SportsFinderDB")
+        appDatabase = Room.databaseBuilder(context, AppDatabase.class, "SportsApp")
+                .fallbackToDestructiveMigration()
+                .addCallback(new DatabaseCallback())
                 .build();
+
+        resetDatabase();
     }
 
     public static synchronized DatabaseClient getInstance(Context context) {
@@ -25,5 +34,13 @@ public class DatabaseClient {
 
     public AppDatabase getAppDatabase() {
         return appDatabase;
+    }
+
+    private void resetDatabase() {
+        executorService.execute(() -> {
+            appDatabase.clearAllTables();
+            DatabaseCallback callback = new DatabaseCallback();
+            callback.insertTestData(appDatabase.gameDao());
+        });
     }
 }
