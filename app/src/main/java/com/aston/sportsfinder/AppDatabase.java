7a2fda89ff997146693d561f7651d7233833dccb
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-13 17:45:17 +0000

    chore: update db-related files

--- app/src/main/java/com/aston/sportsfinder/AppDatabase.java
+++ app/src/main/java/com/aston/sportsfinder/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 import com.aston.sportsfinder.dao.GameDao;
 import com.aston.sportsfinder.model.Game;
 
-@Database(entities = {Game.class}, version = 5)
+@Database(entities = {Game.class}, version = 6, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract GameDao gameDao();
