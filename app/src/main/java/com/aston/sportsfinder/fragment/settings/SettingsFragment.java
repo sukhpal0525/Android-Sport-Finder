70781f0842df6d3d3d9baea8e6570e4e4d616b26
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-13 17:47:00 +0000

    feature: populate bottom nav with new fragment

---
+++ app/src/main/java/com/aston/sportsfinder/fragment/settings/SettingsFragment.java
@@ -0,0 +1,30 @@
+package com.aston.sportsfinder.fragment.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.aston.sportsfinder.databinding.FragmentNotificationsBinding;
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
