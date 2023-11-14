bdfc08f5d31ffd94d8f23981728a26690d968a49
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-14 01:54:59 +0000

    feature: implement clickable map markers

--- app/src/main/java/com/aston/sportsfinder/fragment/search/GameDetailsBottomSheet.java
+++ app/src/main/java/com/aston/sportsfinder/fragment/search/GameDetailsBottomSheet.java
@@ -1,13 +1,18 @@
 package com.aston.sportsfinder.fragment.search;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.aston.sportsfinder.R;
 import com.aston.sportsfinder.model.Game;
@@ -32,13 +37,45 @@ public class GameDetailsBottomSheet extends BottomSheetDialogFragment {
         TextView tvTeamInfo = view.findViewById(R.id.tvTeamInfo);
         TextView tvScore = view.findViewById(R.id.tvScore);
         TextView tvAdditionalInfo = view.findViewById(R.id.tvAdditionalInfo);
-
+        TextView tvStatus = view.findViewById(R.id.tvStatus);
+        Button btnAction = view.findViewById(R.id.btnAction);
 
         tvTeamInfo.setText(game.getTeam1() + " vs " + game.getTeam2());
         tvScore.setText("Score: " + game.getScore1() + " - " + game.getScore2());
         tvGameDetails.setText("Location: " + game.getStreet() + ", " + game.getCity() + "\nDate: " + game.getDate() + "\nTime: " + game.getTime());
         tvAdditionalInfo.setText("Additional Info: Weather, Audience, etc.");
+        String scoreText = game.isStarted() ? "Score: " + game.getScore1() + " - " + game.getScore2() : "Not started";
+        tvScore.setText(scoreText);
 
+        if (game.isStarted()) {
+            tvStatus.setText("Status: Ended");
+            btnAction.setText("Ended");
+            btnAction.setEnabled(false);
+            btnAction.setBackgroundColor(Color.GRAY);
+            btnAction.setTextColor(Color.DKGRAY);
+        } else {
+            tvStatus.setText("Status: Not started");
+            btnAction.setText("Join Game");
+            btnAction.setEnabled(true);
+            btnAction.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.green_light));
+        }
+
+        if (!game.isStarted()) {
+            btnAction.setOnClickListener(v -> {
+                dismiss();
+                JoinGameBottomSheet joinGameBottomSheet = JoinGameBottomSheet.newInstance();
+                joinGameBottomSheet.show(getParentFragmentManager(), "JoinGameBottomSheet");
+            });
+        }
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Close current bottom sheet
+        ImageView ivClose = view.findViewById(R.id.ivClose);
+        ivClose.setOnClickListener(v -> dismiss());
     }
 }
