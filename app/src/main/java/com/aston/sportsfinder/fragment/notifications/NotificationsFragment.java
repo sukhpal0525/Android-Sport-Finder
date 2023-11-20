74a02272ae4755871f3fd1a72e357147dfdb25a6
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-20 19:38:04 +0000

    feature: setup notification viewholder and RecyclerView

--- app/src/main/java/com/aston/sportsfinder/fragment/notifications/NotificationsFragment.java
+++ app/src/main/java/com/aston/sportsfinder/fragment/notifications/NotificationsFragment.java
@@ -1,27 +1,54 @@
 package com.aston.sportsfinder.fragment.notifications;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.aston.sportsfinder.databinding.FragmentNotificationsBinding;
+import com.aston.sportsfinder.model.viewmodel.notifications.NotificationsAdapter;
+import com.aston.sportsfinder.model.viewmodel.notifications.NotificationsViewModel;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private NotificationsAdapter adapter;
+    private NotificationsViewModel viewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        adapter = new NotificationsAdapter();
+        binding.notificationsRecyclerView.setAdapter(adapter);
+        //This is just so the recycler view knows how to position the notification items
+        binding.notificationsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication())).get(NotificationsViewModel.class);
+        viewModel.getNotifications().observe(getViewLifecycleOwner(), notifications -> {
+            Log.d("TEST", "Notifications received: " + notifications.size());
+            if (notifications != null && !notifications.isEmpty()) {
+                adapter.setNotifications(notifications);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        viewModel.refreshNotifications();;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
