d21447657eda8bb3a0ec514197f3dd89ba75e386
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-20 19:23:19 +0000

    chore: remove redundant helper class

--- app/src/main/java/com/aston/sportsfinder/util/NavigationUtil.java
+++ app/src/main/java/com/aston/sportsfinder/util/NavigationUtil.java
@@ -4,10 +4,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavigationUtil {
 
-    // Deselect bottom navigation highlighting
     public static void deselectBottomNavItems(BottomNavigationView bottomNavView) {
-        for (int i = 0; i < bottomNavView.getMenu().size(); i++) {
-            bottomNavView.getMenu().getItem(i).setChecked(false);
         }
     }
-}
