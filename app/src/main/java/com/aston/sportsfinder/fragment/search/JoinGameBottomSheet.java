5e748b4ec9a87456edc8abeddc85d5a633f7b70e
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-20 19:49:18 +0000

    feature: implement game creation
    - Associate user with the joined game.
    - Alert on successful and incorrect actions.
    - Update db and persist the user's joined games.
    - Update Bottom Sheet with close button & listener

--- app/src/main/java/com/aston/sportsfinder/fragment/search/JoinGameBottomSheet.java
+++ app/src/main/java/com/aston/sportsfinder/fragment/search/JoinGameBottomSheet.java
@@ -2,30 +2,44 @@ package com.aston.sportsfinder.fragment.search;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.aston.sportsfinder.R;
-import com.aston.sportsfinder.model.AppUser;
+import com.aston.sportsfinder.dao.GameDao;
 import com.aston.sportsfinder.model.Game;
 import com.aston.sportsfinder.model.Notification;
+import com.aston.sportsfinder.model.viewmodel.notifications.NotificationsViewModel;
 import com.aston.sportsfinder.util.DatabaseClient;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class JoinGameBottomSheet extends BottomSheetDialogFragment {
 
     private Game selectedGame;
+    private ExecutorService executorService;
 
     public static JoinGameBottomSheet newInstance(Game game) {
         JoinGameBottomSheet fragment = new JoinGameBottomSheet();
         fragment.selectedGame = game;
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        executorService = DatabaseClient.getInstance(requireContext()).executorService;
     }
 
     @Override
@@ -37,24 +51,60 @@ public class JoinGameBottomSheet extends BottomSheetDialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Close current bottom sheet
         ImageView ivClose = view.findViewById(R.id.ivClose);
-        ivClose.setOnClickListener(v -> dismiss());
-
         Button btnConfirm = view.findViewById(R.id.btnConfirm);
-        btnConfirm.setOnClickListener(v -> {
-            int userId = AppUser.USER_ID;
-            DatabaseClient.getInstance(getContext()).executorService.execute(() -> {
-                DatabaseClient.getInstance(getContext()).getAppDatabase().gameDao()
-                        .updateGameJoinStatus(selectedGame.getId(), true, userId);
 
-                Notification notification = new Notification(
-                        0, userId, "Joined game: " + selectedGame.getTeam1() + " vs " + selectedGame.getTeam2(),
-                        System.currentTimeMillis(), false, selectedGame.getId());
-                DatabaseClient.getInstance(getContext()).getAppDatabase().notificationDao()
-                        .insertNotification(notification);
-            });
+        // Listeners
+        ivClose.setOnClickListener(v -> dismiss());
+        btnConfirm.setOnClickListener(v -> joinGame());
+    }
+
+    private void insertNotification(int userId) {
+        executorService.execute(() -> {
+            String message = "Joined game: " + selectedGame.getTeam1() + " vs " + selectedGame.getTeam2() +
+                    "\nDate: " + selectedGame.getDate() +
+                    "\nTime: " + selectedGame.getTime();
+
+            Notification notification = new Notification(0, userId, message, System.currentTimeMillis(), false, selectedGame.getId());
+            DatabaseClient.getInstance(getContext()).getAppDatabase().notificationDao().insertNotification(notification);
+            Log.d("TEST-NOTIFICATION", "Notification created for game ID: " + selectedGame.getId());
+
+            NotificationsViewModel viewModel = new ViewModelProvider(requireActivity()).get(NotificationsViewModel.class);
+            viewModel.refreshNotifications();
+        });
+    }
+
+    private void joinGame() {
+        executorService.execute(() -> {
+            try {
+                Log.d("TEST-NOTIFICATION", "Confirm button clicked for game ID: " + selectedGame.getId());
+                Integer userId = DatabaseClient.getInstance(getContext()).getAppDatabase().userDao().getCurrentUserId();
+
+                if (userId != null && !selectedGame.isStarted()) {
+                    processGameJoin(userId);
+                } else {
+                    notify("Cannot join, game already started or User ID is null");
+                }
+            } catch (Exception e) {
+                Log.e("TEST-NOTIFICATION", "Error joining game", e);
+                notify("Error joining game");
+            }
             dismiss();
         });
+    }
+
+    private void processGameJoin(int userId) {
+        GameDao gameDao = DatabaseClient.getInstance(getContext()).getAppDatabase().gameDao();
+        if (!gameDao.isGameJoinedByUser(selectedGame.getId(), userId)) {
+            gameDao.updateGameJoinStatus(selectedGame.getId(), true, userId);
+            insertNotification(userId);
+            notify("Joined game successfully!");
+        } else {
+            notify("You have already joined this game.");
+        }
+    }
+
+    private void notify(String message) {
+        getActivity().runOnUiThread(() -> Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show());
     }
 }
