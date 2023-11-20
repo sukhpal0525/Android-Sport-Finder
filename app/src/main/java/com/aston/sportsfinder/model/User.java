23085fd34f655e793a90027392a40647072b4f2c
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-20 19:26:35 +0000

    chore: populate User with getters/setters

--- app/src/main/java/com/aston/sportsfinder/model/User.java
+++ app/src/main/java/com/aston/sportsfinder/model/User.java
@@ -15,4 +15,34 @@ public class User {
 
     @ColumnInfo(name = "LastName")
     private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
