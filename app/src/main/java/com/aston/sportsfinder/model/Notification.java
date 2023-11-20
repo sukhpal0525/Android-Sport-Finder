1c91382c952033dfaee45c5e0bb72dae568e6233
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-20 19:26:21 +0000

    chore: populate Notification with getters/setters

--- app/src/main/java/com/aston/sportsfinder/model/Notification.java
+++ app/src/main/java/com/aston/sportsfinder/model/Notification.java
@@ -15,6 +15,54 @@ public class Notification {
     private boolean isRead;
     private int gameId;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
     public Notification(int id, int userId, String message, long timestamp, boolean isRead, int gameId) {
         this.id = id;
         this.userId = userId;
