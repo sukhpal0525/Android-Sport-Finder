fbfcffbd7ec8878ab425120e5fb17ca245f7bbb9
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-11-20 19:19:23 +0000

    chore: update AppDatabase to include Notification as a db entity

--- app/src/main/java/com/aston/sportsfinder/AppDatabase.java
+++ app/src/main/java/com/aston/sportsfinder/AppDatabase.java
@@ -5,13 +5,16 @@ import androidx.room.RoomDatabase;
 
 import com.aston.sportsfinder.dao.GameDao;
 import com.aston.sportsfinder.dao.NotificationDao;
+import com.aston.sportsfinder.dao.UserDao;
 import com.aston.sportsfinder.model.Game;
 import com.aston.sportsfinder.model.Notification;
+import com.aston.sportsfinder.model.User;
 
-@Database(entities = {Game.class, Notification.class}, version = 8, exportSchema = false)
+@Database(entities = {Game.class, User.class, Notification.class}, version = 29, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract GameDao gameDao();
+    public abstract UserDao userDao();
     public abstract NotificationDao notificationDao();
 
 }
